5973244d5f942d880f4aa157ea701b76178a3d06
rumwei <isguwei@163.com>
2019-01-09 23:32:03 +0800

    jvm MetaSpace test

--- src/com/rumwei/func/jvmtest/MetaSpaceTest.java
+++
@@ -1,5 +0,0 @@
-package com.rumwei.func.jvmtest;
-
-public class MetaSpaceTest {
-
-}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,12 @@
+import com.rumwei.func.jvmtest.MetaSpaceTest;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        //new MetaSpaceTest().test();
+
+
         System.out.println("Hello World!");
     }
 }
