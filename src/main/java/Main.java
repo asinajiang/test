f899ced4a0f63dd3bf921395233d2b839e207df7
rumwei <isguwei@163.com>
2019-11-24 16:19:18 +0800

    test chinese encode

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,16 @@
 
 
+import com.rumwei.func.Entity;
+import org.apache.commons.cli.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.awt.*;
 
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.util.Enumeration;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -24,10 +30,36 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        String mi = "name";
-        System.out.println(mi);
-        System.out.println(mi);
-        System.out.println(mi);
+
+        //中文提交测试
+        String[] arg = {"-h","-c","usr/local/config.xml","-p"};
+        Options options = new Options();
+        Option op1 = new Option("h", "help", false, "Print help");
+        op1.setRequired(false);
+        options.addOption(op1);
+        Option op2 = new Option("c", "configFile", true, "config file path");
+        op2.setRequired(false);
+        options.addOption(op2);
+
+        CommandLine commandLine = null;
+        CommandLineParser parser = new PosixParser();
+        try{
+            commandLine = parser.parse(options,arg);
+            if (commandLine.hasOption('h')){
+                // 若命令行接了-h，则需要执行的逻辑
+                System.out.println("handle logic responding h");
+            }
+            if (commandLine.hasOption('c')){
+                String configFilePath = commandLine.getOptionValue('c');
+                System.out.println(configFilePath);
+            }
+            if (commandLine.hasOption('p')){
+                System.out.println();
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
 
         System.out.println("start");
 
---
+++ src/main/java/com/rumwei/func/Entity.java
@@ -0,0 +1,23 @@
+package com.rumwei.func;
+
+public class Entity {
+
+    private String ss;
+    private int port;
+
+    public String getSs() {
+        return ss;
+    }
+
+    public void setSs(String ss) {
+        this.ss = ss;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+}
