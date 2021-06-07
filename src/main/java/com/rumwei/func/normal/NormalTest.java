91b4a5f22146e8e85408b0604e1f926197e9724c
guwei <guwei@fenbi.com>
2021-06-07 13:24:02 +0800

    thread

--- src/main/java/com/rumwei/func/normal/NormalTest.java
+++ src/main/java/com/rumwei/func/normal/NormalTest.java
@@ -1,18 +1,12 @@
 package com.rumwei.func.normal;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class NormalTest {
-
-    private static int i = 100;
-
     public static void main(String[] args){
-        NormalTest nt1 = new NormalTest();
-        nt1.i++;
-        NormalTest nt2 = new NormalTest();
-        nt2.i++;
-        nt1 = new NormalTest();
-        nt1.i++;
-        NormalTest.i--;
-        System.out.println("i="+i);
+        for (int index = 0; index < 10; index++) {
+            log.info("print {}", index);
+        }
     }
-
 }
